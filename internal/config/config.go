b4fca7f72b6056df52dd59624946abbe8b80f021
Alexander Hansen <ahansen1335@gmail.com>
2025-01-05 10:09:47 +0100

    setup config Read function for the .gatorconfig.json file in the home directory

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,47 @@
 package config
 
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	DbURL       string `json:"db_url"`
 	CurrentUser string `json:"current_user_name"`
 }
+
+const configFileName = "/.gatorconfig.json"
+
+func Read() (Config, error) {
+	configFilePath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	jsonConfigPath := configFilePath + configFileName
+
+	readJSONConfig, err := os.ReadFile(jsonConfigPath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	var config Config
+	decoder := json.NewDecoder(bytes.NewReader(readJSONConfig))
+	if err := decoder.Decode(&config); err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homePath, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	return homePath, nil
+
+}
+
--- main.go
+++ main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+
+	"github.com/Bones1335/gator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(cfg)
+
+}
